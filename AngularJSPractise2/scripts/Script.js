4cabb87464a748b3e8b821b1a5df15176edc9ce6
Weaveryxl <weaveryxl549@gmail.com>
2017-05-19 16:25:36 -0400

    Revert "Handling Events"
    This reverts commit 6e65bca2af9ecefb171f612fcad64ce2dee01df5.

--- AngularJSPractise2/scripts/Script.js
+++ AngularJSPractise2/scripts/Script.js
@@ -3,20 +3,78 @@
 var app = angular
                 .module("myModule", [])
                 .controller("myController", function ($scope) {
-                    var technologies = [
-                        {name: "C#", likes: 0, dislikes: 0},
-                        { name: "ASP.NET", likes: 0, dislikes: 0 },
-                        { name: "SQL Server", likes: 0, dislikes: 0 },
-                        { name: "AngularJS", likes: 0, dislikes: 0 }
-                    ]
+                    var employees = [
+                        {
+                            firstName: "Ningyu",
+                            lastName: "Zong",
+                            gender: "Female",
+                            fraction: "Zong",
+                        },
+                        {
+                            firstName: "Ningrong",
+                            lastName: "Wanyan",
+                            gender: "Female",
+                            fraction: "Yulong",
+                        },
+                        {
+                            firstName: "Jing",
+                            lastName: "Wen",
+                            gender: "Female",
+                            fraction: "Mingfeng",
+                        },
+                        {
+                            firstName: "Zheng",
+                            lastName: "Wanyan",
+                            gender: "Male",
+                            fraction: "Yulong",
+                        }
+                    ];
 
-                    $scope.technologies = technologies;
+                    var figures = [
+                        {
+                            firstName: "Ningyu",
+                            lastName: "Zong",
+                            gender: "Female",
+                            fraction: "Zong",
+                            related: [
+                                { name: "Ningrong", relationship: "Sister" },
+                                { name: "ZongShuai", relationship: "Husband" },
+                            ]
+                        },
+                        {
+                            firstName: "Ningrong",
+                            lastName: "Wanyan",
+                            gender: "Female",
+                            fraction: "Yulong",
+                            related: [
+                                { name: "Ningyu", relationship: "Sister" },
+                                { name: "Zheng", relationship: "Son" },
+                                { name: "Zang", relationship: "Husband" }
+                            ]
+                        },
+                        {
+                            firstName: "Jing",
+                            lastName: "Wen",
+                            gender: "Female",
+                            fraction: "Mingfeng",
+                            related: [
+                                { name: "Zheng", relationship: "Boyfriend" },
+                                { name: "Taiji", relationship: "Father" },
+                            ]
+                        },
+                        {
+                            firstName: "Zheng",
+                            lastName: "Wanyan",
+                            gender: "Male",
+                            fraction: "Yulong",
+                            related: [
+                                { name: "Jing", relationship: "Girlfriend" },
+                                { name: "Ningrong", relationship: "Mother" },
+                                { name: "Zang", relationship: "Father" }
+                            ]
+                        }
+                    ];
 
-                    $scope.incrementLikes = function (technology) {
-                        technology.likes++;
-                    }
-
-                    $scope.incrementDislikes = function (technology) {
-                        technology.dislikes++;
-                    }
+                    $scope.employees = employees;
+                    $scope.figures = figures;
                 });
