618b6b1f80e82181bdaf01d481a559f47981528e
Weaveryxl <weaveryxl549@gmail.com>
2017-05-19 17:20:48 -0400

    AngularJS Filters

--- AngularJSPractise2/scripts/Script.js
+++ AngularJSPractise2/scripts/Script.js
@@ -4,10 +4,10 @@ var app = angular
                 .module("myModule", [])
                 .controller("myController", function ($scope) {
                     var technologies = [
-                        {name: "C#", likes: 0, dislikes: 0},
-                        { name: "ASP.NET", likes: 0, dislikes: 0 },
-                        { name: "SQL Server", likes: 0, dislikes: 0 },
-                        { name: "AngularJS", likes: 0, dislikes: 0 }
+                        {name: "C#", likes: 0, dislikes: 0, salary: 60000, startDate: new Date("November 23, 1980")},
+                        { name: "ASP.NET", likes: 0, dislikes: 0, salary: 70000, startDate: new Date("May 05, 1970") },
+                        { name: "SQL Server", likes: 0, dislikes: 0, salary: 60000, startDate: new Date("August 15, 1974") },
+                        { name: "AngularJS", likes: 0, dislikes: 0, salary: 65000, startDate: new Date("December 30, 1983") }
                     ]
 
                     $scope.technologies = technologies;
@@ -19,4 +19,5 @@ var app = angular
                     $scope.incrementDislikes = function (technology) {
                         technology.dislikes++;
                     }
+                    $scope.rowLimit = 2;
                 });
