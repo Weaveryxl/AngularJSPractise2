c5c8da45c5dfc22c6793271af1911915e2fa1cd7
Weaveryxl <weaveryxl549@gmail.com>
2017-05-22 16:07:37 -0400

    $http service in AngularJS

--- AngularJSPractise2/scripts/Script.js
+++ AngularJSPractise2/scripts/Script.js
@@ -2,9 +2,21 @@
 
 var app = angular
                 .module("myModule", [])
-                .controller("myController", function ($scope, $http) {
-                    $http.get('EmployeeService.asmx/GetAllEmployees')
-                            .then(function (response) {
-                                $scope.employees = response.data;
-                            });
+                .controller("myController", function ($scope, $http, $log) {
+
+                    var successCallBack = function (response) {
+                        $scope.employees = response.data;
+                        $log.info(response);
+                    };
+
+                    var errorCallBack = function (response) {
+                        $scope.error = response.data;
+                        $log.info(response);
+                    }
+
+                    $http({
+                            method: 'GET', 
+                            url: 'EmployeeService.asmx/GetAllEmployees'
+                    })
+                        .then(successCallBack, errorCallBack);
                 });
