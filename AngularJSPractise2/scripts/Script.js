6ae19bc140953929b3159cd679b88f043ae28627
Weaveryxl <weaveryxl549@gmail.com>
2017-05-22 11:20:39 -0400

    Consuming ASP NET Web Service in AngularJS using $http, and if using get request, we need  to add httpGet in web.config

--- AngularJSPractise2/scripts/Script.js
+++ AngularJSPractise2/scripts/Script.js
@@ -2,15 +2,9 @@
 
 var app = angular
                 .module("myModule", [])
-                .controller("myController", function ($scope) {
-                    var employees = [
-                        { name: 'Ben', gender: 'Male', city: 'London', salary: '55000' },
-                        { name: 'Sara', gender: 'Female', city: 'Chennai', salary: '68000' },
-                        { name: 'Mark', gender: 'Male', city: 'Chicago', salary: '57000' },
-                        { name: 'Pam', gender: 'Female', city: 'London', salary: '53000' },
-                        { name: 'Todd', gender: 'Male', city: 'Chennai', salary: '60000' }
-                    ];
-
-                    $scope.employees = employees;
-                    $scope.employeeView = "EmployeeTable.html";
+                .controller("myController", function ($scope, $http) {
+                    $http.get('EmployeeService.asmx/GetAllEmployees')
+                            .then(function (response) {
+                                $scope.employees = response.data;
+                            });
                 });
