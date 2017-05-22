23e3723caba25f7365177ef5833c8a784f63ddc0
Weaveryxl <weaveryxl549@gmail.com>
2017-05-22 16:47:15 -0400

    Create custom service in AngularJS, 19 and 20

--- AngularJSPractise2/scripts/Script.js
+++ AngularJSPractise2/scripts/Script.js
@@ -2,21 +2,8 @@
 
 var app = angular
                 .module("myModule", [])
-                .controller("myController", function ($scope, $http, $log) {
-
-                    var successCallBack = function (response) {
-                        $scope.employees = response.data;
-                        $log.info(response);
-                    };
-
-                    var errorCallBack = function (response) {
-                        $scope.error = response.data;
-                        $log.info(response);
+                .controller("myController", function ($scope, stringService) {
+                    $scope.transformString = function (input) {
+                        $scope.output = stringService.processString(input);
                     }
-
-                    $http({
-                            method: 'GET', 
-                            url: 'EmployeeService.asmx/GetAllEmployees'
-                    })
-                        .then(successCallBack, errorCallBack);
                 });
