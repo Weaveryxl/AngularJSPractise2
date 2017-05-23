81dab20f419d760a78191d621bc6952ecaef6c74
Weaveryxl <weaveryxl549@gmail.com>
2017-05-23 09:58:43 -0400

    AngularJS anchorscroll example

--- AngularJSPractise2/scripts/Script.js
+++ AngularJSPractise2/scripts/Script.js
@@ -7,3 +7,13 @@ var app = angular
                         $scope.output = stringService.processString(input);
                     }
                 });
+
+var demoApp = angular
+                    .module("demoApp", [])
+                    .controller("demoController", function ($scope, $location, $anchorScroll) {
+                        $scope.scrollTo = function (scrollLocation) {
+                            $location.hash(scrollLocation);
+                            $anchorScroll.yOffset = 20;
+                            $anchorScroll();
+                        }
+                    });
