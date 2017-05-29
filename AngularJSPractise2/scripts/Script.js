2b88abd8c37c3e6ce73d798ffbd3da3bea2775de
Weaveryxl <weaveryxl549@gmail.com>
2017-05-29 17:45:18 -0400

    Angularjs route configuration, a bug about angularJS 1.6+ and angularJS-route showed up, but provided two ways to solve it in the index.html and Script.js

--- AngularJSPractise2/scripts/Script.js
+++ AngularJSPractise2/scripts/Script.js
@@ -1,24 +1,55 @@
 ﻿/// <reference path="angular.js" />
 
-var app = angular
-                .module("myModule", [])
-                .controller("myController", function ($scope, stringService) {
-                    $scope.transformString = function (input) {
-                        $scope.output = stringService.processString(input);
-                    }
-                });
+//var app = angular
+//                .module("myModule", [])
+//                .controller("myController", function ($scope, stringService) {
+//                    $scope.transformString = function (input) {
+//                        $scope.output = stringService.processString(input);
+//                    }
+//                });
 
-var demoApp = angular
-                    .module("demoApp", [])
-                    .controller("demoController", function ($scope, $http, $location, $anchorScroll) {
-                        $http.get('CountryService.asmx/GetData')
-                        .then(function (response) {
-                            $scope.countries = response.data;
+//var demoApp = angular
+//                    .module("demoApp", [])
+//                    .controller("demoController", function ($scope, $http, $location, $anchorScroll) {
+//                        $http.get('StudentService.asmx/GetAllData')
+//                        .then(function (response) {
+//                            $scope.students = response.data;
+//                        })
+
+//                        $scope.scrollTo = function (studentName) {
+//                            $location.hash(studentName);
+//                            $anchorScroll.yOffset = 20;
+//                            $anchorScroll();
+//                        }
+//                    });
+
+var routeApp = angular
+                    .module("routeApp", ["ngRoute"])
+                    .config(function ($routeProvider, $locationProvider) {
+                        $locationProvider.hashPrefix('');
+                        $routeProvider
+                            .when("/home", {
+                                templateUrl: "Templates/home.html",
+                                controller: "homeController"
+                            })
+                            .when("/courses", {
+                                templateUrl: "Templates/Course.html",
+                                controller: "coursesController"
+                            })
+                            .when("/students", {
+                                templateUrl: "Templates/Student.html",
+                                controller: "studentsController"
+                            })
                         })
-
-                        $scope.scrollTo = function (countryName) {
-                            $location.hash(countryName);
-                            $anchorScroll.yOffset = 20;
-                            $anchorScroll();
-                        }
-                    });
+                        .controller("homeController", function ($scope) {
+                            $scope.message = "Home Page";
+                        })
+                        .controller("coursesController", function ($scope) {
+                            $scope.courses = ["C#", "VB.NET", "SQL Server", "ASP.NET"];
+                        })
+                        .controller("studentsController", function ($scope, $http) {
+                            $http.get('StudentService.asmx/GetAllStudents')
+                            .then(function (response) {
+                                $scope.students = response.data;
+                            })
+                        });
